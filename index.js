2ad889187660e8e10b982088ad69ffc551184a17
Jesus <you@example.com>
2023-05-17 19:12:35 -0600

    index.js agregado

---
+++ index.js
@@ -0,0 +1,5 @@
+// create a function that returns the index of 'coconut' of a given array or in case 'coconut' 
+// is not in the array the function will push it onto the array
+
+const arr = ['carrot', 'cheese', 'coconut']  // 2
+const arr2 = ['carrot', 'apple', 'orange'] //  ['carrot', 'apple', 'orange', 'coconut]
